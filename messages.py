ea5a99f1f1985d34312722a27224b35f10c498ef
KumarGN69 <kumargn1969@hotmail.com>
2025-06-08 18:09:57 +0000

    with tool binding

--- messages.py
+++ messages.py
@@ -3,6 +3,14 @@ from langchain_core.messages import AIMessage, HumanMessage,SystemMessage, BaseM
 from custom_llm import CustomLLMModel
 from langchain_ollama import ChatOllama
 
+def multiply(a:int, b:int) ->int:
+    """
+    Multiply a and b
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
 model = ChatOllama(model = "mistral:latest")
 
 messages = [AIMessage(content="Hi")]
@@ -11,6 +19,7 @@ messages.append(HumanMessage(content=f"Yes that's right!", name="Kumar"))
 messages.append(AIMessage(content=f"What part of Integral calculus do you want to learn", name="Model"))
 messages.append(HumanMessage(content=f"I want to learn about basics!", name="Kumar"))
 
-result = model.invoke(messages)
+model_with_tools = model.bind_tools([multiply])
+result = model_with_tools.invoke([HumanMessage(content=f"What is 3 multipled by 10", name="Kumar")])
 # print(type(result))
 pprint(result)
