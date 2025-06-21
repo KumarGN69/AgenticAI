fa70163add3cc2f7665c1e362ee12468bb9cc5ef
kumarGN69 <kumargn1969@otmail.com>
2025-06-21 16:23:29 -0500

    with reducers

--- custom_reducers.py
+++ custom_reducers.py
@@ -1,21 +1,22 @@
-from typing import TypedDict, Literal
+from typing import TypedDict, Literal, Annotated
+from operator import add
 import random
 from langgraph.graph import StateGraph , START, END
 from pydantic import BaseModel
 from langgraph.errors import InvalidUpdateError
 
 class PydanticState(BaseModel):
-    foo: int
+    foo: Annotated[list[int],add]
 
 def node_1(state: PydanticState):
     print("--Node1--")
-    return {"foo": state.foo + 1}
+    return {"foo": [state.foo[0] + 1]}
 def node_2(state: PydanticState):
     print("--Node2--")
-    return {"foo": state.foo+ 2}
+    return {"foo": [state.foo[0] + 2]}
 def node_3(state:PydanticState):
     print("--Node3--")
-    return {"foo": state.foo + 3}
+    return {"foo": [state.foo[0] + 3]}
 
 builder = StateGraph(PydanticState)
 builder.add_node("node_1", node_1)
@@ -32,7 +33,8 @@ graph = builder.compile()
 with open("graph.png", "wb") as f:
     f.write(graph.get_graph().draw_mermaid_png())
 try:
-    result = graph.invoke(PydanticState(foo=0))
+    result = graph.invoke(PydanticState(foo=[1]))
+    print("Final result:", result["foo"])
 except InvalidUpdateError as e:
     print(f"Invalid update error occurred: {e}")
     # Should print the final value of foo after processing through the nodes
