9caec4b908a462f3e78b66da383ec8c3db5ce33f
KumarGN69 <kumargn1969@hotmail.com>
2025-10-05 17:43:31 +0530

    code refactored using continue dev

--- custom_dspy.py
+++ custom_dspy.py
@@ -1,11 +1,18 @@
 import dspy
 
-lm = dspy.LM(model='ollama_chat/mistral:latest',api_base='http://localhost:11434',api_key='')
-dspy.configure(lm=lm)
+class DSPY:
+    def __init__(self):
+        self.lm = dspy.LM(model='ollama_chat/mistral:latest',api_base='http://localhost:11434',api_key='')
+        dspy.configure(lm=self.lm)
 
-qa = dspy.ChainOfThought('question -> answer')
-response = qa(question='Who is the president of USA')
-print(type(response.answer))
-print("Reasoning:",response.reasoning)
-print("Answer:",response.answer)
+    def chainofthought(self, question, answer) -> None:
+        qa = dspy.ChainOfThought('question -> answer')
+        response = qa(question=question, answer=answer)
+        print(type(response.answer))
+        print("Reasoning:",response.reasoning)
+        print("Answer:",response.answer)
+
+# Example usage
+handle = DSPY()
+handle.chainofthought(question='Who is the president of USA', answer='')
 
